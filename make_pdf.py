7c982503fbddfba76538b002aa39f88812099801
Surendra030 <surendrababun030@gmail.com>
2024-12-01 14:45:22 +0530

    NONE..

--- make_pdf.py
+++ make_pdf.py
@@ -3,7 +3,7 @@ from PIL import Image
 from io import BytesIO
 from fpdf import FPDF
 import json
-from mega import Mega
+import dropbox
 import os
 
 # Function to download and save images
@@ -19,7 +19,7 @@ def download_images(images, paper_name):
     return image_paths
 
 # Function to create PDF from images
-def create_pdf(image_paths,title):
+def create_pdf(image_paths, title):
     pdf = FPDF()
     pdf.set_auto_page_break(auto=True, margin=5)
     
@@ -33,24 +33,28 @@ def create_pdf(image_paths,title):
     
     return pdf_output_path
 
-# Function to upload the PDF to Mega
-def upload_to_mega(pdf_path, email, password):
-    # Log in to Mega
-    mega = Mega()
-    print("new connection establing")
-    m = mega.login(email, password)
-    print("Uploading to Mega cloud...")
-    # Upload the PDF file to Mega
-    m.upload(pdf_path)
-
-
-def main_pdf(data,title):
+# Function to upload the PDF to Dropbox
+def upload_to_dropbox(pdf_path, folder_name="/light-novels"):
+    dropbox_token = os.getenv("M_TOKEN")
+    # Connect to Dropbox
+    dbx = dropbox.Dropbox(dropbox_token)
+    print("Uploading to Dropbox...")
     
+    # Open the PDF file and upload to Dropbox
+    with open(pdf_path, "rb") as f:
+        file_data = f.read()
+        # Upload file to specified folder in Dropbox
+        dbx.files_upload(file_data, folder_name + "/" + os.path.basename(pdf_path), mode=dropbox.files.WriteMode.overwrite)
+
+    print("Upload complete!")
+
+def main_pdf(data, title):
     paper_list = []
 
     for list_item_obj in data:
         for key in list_item_obj:
             paper_list.append(key)
+
     # Download images from all papers
     image_paths = []
 
@@ -65,12 +69,11 @@ def main_pdf(data,title):
             image_paths.extend(download_images(images, paper_name))
 
     # Create the PDF
-    pdf_path = create_pdf(image_paths,title)
+    pdf_path = create_pdf(image_paths, title)
 
-    # Upload the PDF to Mega Cloud
-    email = "afg154004@gmail.com"  # Replace with your Mega email
-    password = "megaMac02335!"  # Replace with your Mega password
-    upload_to_mega(pdf_path, email, password)
+    # Upload the PDF to Dropbox
+    upload_to_dropbox(pdf_path)
 
     # Optional: Remove the locally saved PDF file after uploading
     os.remove(pdf_path)
+
