649e2c1b5329f8c48988dd4b52a37755a59eb6fe
Surendra030 <surendrababun030@gmail.com>
2024-12-03 23:18:44 +0530

    two..

--- main_file.py
+++ main_file.py
@@ -9,7 +9,7 @@ url = "https://anyflip.com/explore?q=Jobless%20reincarnation"
 data = main(url)
 data = [i for i in data  if "jobless" in i['title'].lower()]
 constructed_urls = []
-data= data[:21]
+data= data[21:41]
 
 def sanitize_title(title):
     
