54e42513aa88c06080574a122908ec27bcb54022
Surendra030 <surendrababun030@gmail.com>
2024-12-04 12:02:14 +0530

    none..

--- save_urls_only.py
+++ save_urls_only.py
@@ -7,10 +7,10 @@ def login_data(title,index,data):
     password = 'megaMac02335!'
     mega = Mega()
     m = mega.login(email,password)
-    title = f"{index}_title"
+    title = f"{index}_title.json"
     folder = m.create_folder(title)
     folder_handle = folder.get(title)
-    m.upload(f"{title}.json",folder_handle)
+    m.upload(f"{title}",folder_handle)
 def main_fun(data, title,index):
     with open(title,'w',encoding='utf-8')as f:
         json.dump(data,f,indent=4)
