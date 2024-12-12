5d80456771e01c3212e253ffadda5370c35f0098
Surendra030 <surendrababun030@gmail.com>
2024-12-12 23:15:13 +0530

    debuggin on line 80.

--- full_video.py
+++ full_video.py
@@ -9,16 +9,14 @@ import os
 # add_audio_every_10_minutes("video.mp4", "audio.mp3", "output_video.mp4")
 
 def login_part(m=None):
-    count = 0
     keys = os.getenv("M_TOKEN")
     keys = keys.split("_")
-    [email,password] = keys
     if m:
         return m
     else:
         try:
             mega = Mega()
-            m = mega.login(email,password)
+            m = mega.login(keys[0],keys[1])
             return m
         except Exception as e:
             print("login failed training again..")
@@ -27,42 +25,66 @@ def login_part(m=None):
                 login_part()
 
 
-def get_or_create_folder(m,all_folders, main_folder_name):
+def get_or_create_folder(m,all_folders= None, main_folder_name = None):
     """Retrieve an existing folder by main_folder_name or create a new one."""
-    try:
-        print(f"Checking for the existence of the folder '{main_folder_name}'...")
+    if all_folders and main_folder_name:
+        try:
+            print(f"Checking for the existence of the folder '{main_folder_name}'...")
 
-        folder_handler = next(
-            (folder for folder in all_folders.values() if folder['a']['n'] == main_folder_name and folder['t'] == 1),
-            None
-        )
+            folder_handler = next(
+                (folder for folder in all_folders.values() if folder['a']['n'] == main_folder_name and folder['t'] == 1),
+                None
+            )
 
 
-        if folder_handler:
-            print(f"Folder '{folder_handler['a']['n']} - ' already exists. Using the existing folder.")
-            return folder_handler['h']
+            if folder_handler:
+                print(f"Folder '{folder_handler['a']['n']} - ' already exists. Using the existing folder.")
+                return folder_handler['h']
+            
+            else:
+
+                print(f"Folder '{main_folder_name}' does not exist. Creating a new folder...")
+                folder = m.create_folder(main_folder_name)
+                folder_handler = folder.get(main_folder_name)
+                print(f"Folder '{main_folder_name}' created successfully.")
+                
+                return folder_handler
+
+        except Exception as e:
+            print(f"An error occurred while handling the folder: {e}")
+            return None
+    else:
+        temp_folder_name = 'temp_folder'
         
-        else:
-            print(f"Folder '{main_folder_name}' does not exist. Creating a new folder...")
-            folder = m.create_folder(main_folder_name)
-            folder_handler = folder.get(main_folder_name)
-            print(f"Folder '{main_folder_name}' created successfully.")
-            return folder_handler
-
-    except Exception as e:
-        print(f"An error occurred while handling the folder: {e}")
-        return None
-
-
+        try:
+            file_obj = m.create_folder(temp_folder_name)
+        
+            return file_obj['h']
+        except Exception as e:
+            print("Error creating temp folder :",e)
 
 def upload_to_mega(file_path,main_folder):
     m = login_part()
     all_folders = m.get_files()
-    folder_handle = get_or_create_folder(m,all_folders,main_folder)
-    file_obj = m.upload(file_path,folder_handle)
-    file_link = m.get_upload_link(file_obj)
 
-    return file_link
+    try:
+        folder_handle = get_or_create_folder(m,all_folders,main_folder)
+        
+        try:
+            file_obj = m.upload(file_path,folder_handle)
+            file_link = m.get_upload_link(file_obj)
+            return  file_link
+        
+        except Exception as e:
+            print(f"Error file uploading to {main_folder}",e)
+            folder_handle = get_or_create_folder(m)
+            file_obj = m.upload(file_path,folder_handle)
+            file_link = m.get_upload_link(file_obj)
+
+            return file_link
+    
+    except Exception as e:
+        print("Error : ",e)
 
 
 
